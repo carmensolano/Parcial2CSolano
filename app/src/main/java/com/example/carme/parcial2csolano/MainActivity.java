646a5a906b9b9b070bc13e6b56f7148cf6f9a0fb
carme <00029613@uca.edu.sv>
2018-06-04 23:19:14 -0600

    Navigation Bar Added

--- app/src/main/java/com/example/carme/parcial2csolano/MainActivity.java
+++ app/src/main/java/com/example/carme/parcial2csolano/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.carme.parcial2csolano;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close );
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+        NavigationView nvDrawer = (NavigationView )findViewById(R.id.nv);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(nvDrawer);
     }
 
     @Override
@@ -29,5 +35,49 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void selectIterDrawer(MenuItem menuItem){
+        Fragment myFragment = null ;
+        Class fragmentClass ;
+        switch (menuItem.getItemId()){
+            case R.id.news:
+                fragmentClass = NewsFragment.class;
+                break;
+            case R.id.games:
+                fragmentClass = GamesFragment.class;
+                break;
+            case R.id.settings:
+                fragmentClass = SettingsFragment.class;
+                break;
+            case R.id.favorites:
+                fragmentClass= FavoritesFragment.class;
+                break;
+            default:
+                fragmentClass= NewsFragment.class;
+
+        }
+
+        try{
+            myFragment =(Fragment)fragmentClass.newInstance();
+        }
+        catch (Exception e){
+          e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame1 , myFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectIterDrawer(item);
+                return true;
+            }
+        });
     }
 }
