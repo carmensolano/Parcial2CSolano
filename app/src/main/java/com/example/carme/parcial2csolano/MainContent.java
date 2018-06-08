37243037bbb9db52956be2676ba80341bc4f5626
carme <00029613@uca.edu.sv>
2018-06-08 01:09:19 -0600

    Main Content

---
+++ app/src/main/java/com/example/carme/parcial2csolano/MainContent.java
@@ -0,0 +1,13 @@
+package com.example.carme.parcial2csolano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainContent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+}
