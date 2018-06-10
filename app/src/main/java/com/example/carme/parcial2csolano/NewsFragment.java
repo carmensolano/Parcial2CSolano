c10b07ab1cd9e866f184c332b64b97b43b71631b
carme <00029613@uca.edu.sv>
2018-06-10 02:36:54 -0600

    News fragments

--- app/src/main/java/com/example/carme/parcial2csolano/NewsFragment.java
+++ app/src/main/java/com/example/carme/parcial2csolano/NewsFragment.java
@@ -1,101 +1,96 @@
 package com.example.carme.parcial2csolano;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NewsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NewsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NewsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public NewsFragment() {
-        // Required empty public constructor
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewsFragment newInstance(String param1, String param2) {
-        NewsFragment fragment = new NewsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    RecyclerView recyclerViewNews;
+    public NewsFragment() { }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view= inflater.inflate(R.layout.fragment_news, container, false);
+        recyclerViewNews= (RecyclerView)view.findViewById(R.id.recycler_news);
+        recyclerViewNews.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+
+    private class RecyclerAdapter extends RecyclerView.Adapter<GridHolder>{
+
+        Bitmap[] bitmaps;
+
+        public RecyclerAdapter(Bitmap[] bitmaps){
+            this.bitmaps = bitmaps(bitmaps);
+        }
+
+
+        @Override
+        public GridHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(NewsFragment.this).inflate(R.layout.news, parent, false);
+
+            return new GridHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull GridHolder holder, int position) {
+            holder.imageView.setImageBitmap(bitmaps[position]);
+            holder.textView.setText("NewsTitle" + position);
+            holder.textView1.setText("NewsBody"+ position);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return bitmaps.length;
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
 
+    private class GridHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textView;
+        TextView textView1;
+
+        public GridHolder(View itemView) {
+            super(itemView);
+            imageView= itemView.findViewById(R.id.newsImage);
+            textView= itemView.findViewById(R.id.newsTitle);
+            textView1= itemView.findViewById(R.id.newsBody);
+        }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
